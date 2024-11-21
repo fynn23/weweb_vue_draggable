e72b3ec45840a303501c7f638656a6f3b1432bd3
fynn23 <54664196+fynn23@users.noreply.github.com>
2024-11-21 16:06:39 +0100

    00

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,44 +1,110 @@
 <template>
-    <div v-if="!items">Please bind variable</div>
     <draggable
-        v-else
-        tag="transition-group"
-        v-model="items"
-        :group="group"
+        v-model="internalItems"
         :item-key="itemKey"
-        :animation="200"
-        ghost-class="ghost"
-        :disabled="isEditing"
-        :component-data="{ tag: 'ul', name: 'flip-list', type: 'transition' }"
+        :clone="(el) => el"
+        :group="group"
+        :sort="sortable"
+        :animation = "200"
+        :handle="handle?.length ? `.${handle}` : null"
+        :disabled="isEditing || isReadonly"
+        @change="onChange"
+        @start="setDrag(true)"
+        @end="setDrag(false)"
     >
-        <template #item="{ element, index }">
-            <div>
-                <wwLayoutItemContext :index="index" :item="{}" is-repeat :data="element">
-                    <wwLayout path="itemContent"></wwLayout>
+        <template #header>
+            <wwLayout path="headerElement"></wwLayout>
+        </template>
+        <template #item="{ element, index: itemIndex }">
+            <div class="draggable-item">
+                <wwLayoutItemContext
+                    :index="itemIndex"
+                    :item="null"
+                    is-repeat
+                    :data="element"
+                    :repeated-items="internalItems"
+                >
+                    <wwLayout path="itemElement"></wwLayout>
                 </wwLayoutItemContext>
             </div>
+        </template>
+        <template #footer>
+            <wwLayout path="footerElement"></wwLayout>
         </template>
     </draggable>
 </template>
 
 <script>
 import draggable from "vuedraggable";
-import { ref } from "vue";
 
 export default {
     components: {
         draggable,
     },
+    inject: {
+        customHandler: { defaultValue: null },
+        customDragHandler: { defaultValue: null },
+    },
     props: {
-        content: { type: Object, required: true },
         wwElementState: { type: Object, required: true },
+        content: { type: Object, required: true },
+        uid: { type: String, required: true },
         /* wwEditor:start */
         wwEditorState: { type: Object, required: true },
         /* wwEditor:end */
     },
-    emits: ["update:content", "update:content:effect", "trigger-event", "element-event"],
-    setup() {
-        return { internalGroup: wwLib.wwUtils.getUid(), items: ref([]) };
+    emits: ["trigger-event"],
+    setup(props) {
+        const { value: isDragging, setValue: setDrag } = wwLib.wwVariable.useComponentVariable({
+            uid: props.uid,
+            name: "isDragging",
+            type: "boolean",
+            defaultValue: false,
+            readonly: true,
+        });
+        return { isDragging, setDrag };
+    },
+    data: () => ({
+        internalItems: [],
+    }),
+    methods: {
+        onChange(change) {
+            this.customHandler &&
+                this.customHandler(change, { ...this.wwElementState.props, updatedStackItems: this.internalItems });
+            if (change.moved) {
+                this.$emit("trigger-event", {
+                    name: "item:moved",
+                    event: {
+                        item: change.moved.element,
+                        oldIndex: change.moved.oldIndex,
+                        newIndex: change.moved.newIndex,
+                        updatedList: this.internalItems,
+                    },
+                });
+            }
+
+            if (change.added) {
+                this.$emit("trigger-event", {
+                    name: "item:added",
+                    event: {
+                        item: change.added.element,
+                        newIndex: change.added.newIndex,
+                        updatedList: this.internalItems,
+                    },
+                });
+            }
+
+            if (change.removed) {
+                this.$emit("trigger-event", {
+                    name: "item:removed",
+                    event: {
+                        item: change.removed.element,
+                        oldIndex: change.removed.oldIndex,
+                        updatedList: this.internalItems,
+                    },
+                });
+            }
+        },
     },
     computed: {
         isEditing() {
@@ -48,45 +114,74 @@ export default {
             // eslint-disable-next-line no-unreachable
             return false;
         },
-        items: {
-            get() {
-                if (!this.content.variableId && !(this.wwElementState.props && this.wwElementState.props.items)) {
-                    return null;
-                }
-                const data =
-                    (this.wwElementState.props && this.wwElementState.props.items) ||
-                    wwLib.wwVariable.getValue(this.content.variableId);
-                if (!Array.isArray(data)) {
-                    return null;
-                } else {
-                    return data;
-                }
-            },
-            set(items) {
-                this.$emit("trigger-event", { name: "change", event: { value: items } });
-                this.$emit("element-event", { name: "change", event: { value: items } });
-                if (this.content.variableId) wwLib.wwVariable.updateValue(this.content.variableId, items);
-            },
+        items() {
+            const data = this.wwElementState.props.items ? this.wwElementState.props.items : this.content.items;
+            const items = wwLib.wwCollection.getCollectionData(data);
+            if (!Array.isArray(items)) return [];
+            return items;
         },
         group() {
-            return (this.wwElementState.props && this.wwElementState.props.group) || this.internalGroup;
+            return this.wwElementState.props.group ? this.wwElementState.props.group : this.content.group;
+        },
+        sortable() {
+            return this.wwElementState.props.sortable ? this.wwElementState.props.sortable : this.content.sortable;
         },
         itemKey() {
-            return (this.wwElementState.props && this.wwElementState.props.itemKey) || this.content.itemKey;
+            return this.wwElementState.props.itemKey || "id";
+        },
+        handle() {
+            return this.wwElementState.props.handle?.length
+                ? this.wwElementState.props.handle
+                : this.content.customDragHandle
+                ? this.content.handleClass || "draggable"
+                : null;
+        },
+        isReadonly() {
+            /* wwEditor:start */
+            if (this.wwEditorState.isSelected) {
+                return this.wwElementState.states.includes("readonly");
+            }
+            /* wwEditor:end */
+            // Ensure to return a boolean as vuedraggable interpret undefined as true
+            return !!(this.wwElementState.props.readonly || this.content.readonly);
+        },
+    },
+    watch: {
+        items: {
+            immediate: true,
+            deep: true,
+            handler: function (value) {
+                this.internalItems = [...value];
+            },
+        },
+        isDragging(value) {
+            if (this.customDragHandler) {
+                this.customDragHandler(value, { ...this.wwElementState.props });
+            }
+        },
+        isReadonly: {
+            immediate: true,
+            handler(value) {
+                if (value) {
+                    this.$emit("add-state", "readonly");
+                } else {
+                    this.$emit("remove-state", "readonly");
+                }
+            },
         },
     },
 };
 </script>
 
 <style scoped>
-.flip-list-move {
-  transition: transform 0.5s;
+/** FIX POINTER-EVENTS: ALL BREAKING DRAGGABLE ON MOBILE/TABLET (TOUCH MODE) */
+.draggable-item :deep(.ww-layout) {
+    pointer-events: unset !important;
 }
-.ghost {
-    opacity: 0.5;
-    background: #c8ebfb;
+.draggable-item :deep(* > .ww-element) {
+    pointer-events: unset !important;
 }
-.not-draggable {
-    cursor: no-drop;
+.draggable-item :deep(** > .ww-element) {
+    pointer-events: unset !important;
 }
 </style>
